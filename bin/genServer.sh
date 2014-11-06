cdc5e3d8ea510def4f877dc9023273056b04551c
Martin Ruderer <martin.ruderer@freiheit.com>
2014-11-06 15:57:05 +0100

    slightly improved genServer

--- bin/genServer.sh
+++ bin/genServer.sh
@@ -2,14 +2,10 @@
 
 SCRIPT="$0"
 
-while getopts s:d: opt; do
+while getopts s: opt; do
     case $opt in
     s)
         SOURCEFILE=$OPTARG
-        ;;
-    d)
-        DESTDIR=$OPTARG
-
     esac
 done
 shift $((OPTIND - 1))
@@ -18,11 +14,9 @@ if [ ! -f "$SOURCEFILE" ]; then
     echo "File not found: $SOURCEFILE"
     exit
 fi
+DESTDIR="testserver"
 
-if [ ! -d "$DESTDIR" ]; then
-    echo "Invalid destination: $DESTDIR"
-    exit
-fi
+mkdir $DESTDIR
 
 while [ -h "$SCRIPT" ] ; do
   ls=`ls -ld "$SCRIPT"`
@@ -46,5 +40,20 @@ cd $APP_DIR
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ com.wordnik.swagger.codegen.Codegen -i $SOURCEFILE -l Standalone -o $DESTDIR"
 
+echo "Generating server..."
 echo "java -cp $APP_DIR/target/*:$APP_DIR/target/lib/* $ags"
-java -cp $APP_DIR/target/*:$APP_DIR/target/lib/* $ags
+
+java -cp $APP_DIR/target/*:$APP_DIR/target/lib/* $ags &> /dev/null
+
+cd $DESTDIR
+echo "Compiling server..."
+mvn package &> /dev/null
+cd ..
+
+
+if [ ! -f "$DESTDIR/target/swagger-server-1.0.0-standalone.jar" ] ; then
+    echo "$DESTDIR/target/swagger-server-1.0.0-standalone.jar does not exist. Something went wrong. Call me"
+    exit
+fi
+
+cp "$DESTDIR/target/swagger-server-1.0.0-standalone.jar" .
